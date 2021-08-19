f32f1fb5ca3fbc01fe3fe0f0d03390bfe0d91bf0
Chetan Dhangar <chetansdhangar@gmail.com>
2021-08-19 18:39:24 +0530

    test - making a note and deleting it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+export type Note = {
+  id : number,
+  title : string,
+  description : string
+}
+
+export type NoteType = Note[]
+
 function App() {
+  const [title , setTitle] = useState<string>("")
+  const [description , setDescription] = useState<string>("")
+  const [note , setNote] = useState<NoteType>([])
+  
+  function handleInputChange(e : any){
+    setTitle(e.target.value)
+  }
+  function handleDescriptionChagne(e : any) {
+    setDescription(e.target.value)
+  }
+
+  function addNote(){
+    setNote((note) => note.concat({
+      id : note.length + 1,
+      title ,
+      description
+    }))
+    setTitle("")
+    setDescription("")
+  }
+
+  function handleDeleteNote(noteId : number){
+      setNote(note.filter((note ) => note.id !== noteId))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Note App
+     <input 
+     value={title}
+     placeholder="Title"
+     onChange={handleInputChange}
+     />
+      <textarea
+     value={description}
+     placeholder="Description"
+     onChange={handleDescriptionChagne}
+     />
+     <div>Current title : {title}</div>
+  <div>Description : {description}</div>
+
+  <div>
+    <button onClick={() => addNote()}>Add Note</button>
+  </div>
+  <hr/>
+  <hr/>
+  <div>
+    Notes will be added here :
+    {note.map((note) => (
+      <div>
+        Title : {note.title}
+        Description : {note.description}
+        <button onClick={() => handleDeleteNote(note.id)}>Delete Note</button>
+      </div>
+    ))}
+  </div>
     </div>
   );
 }
